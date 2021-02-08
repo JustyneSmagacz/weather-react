e232f9329f66840f81c1be65f67be64cea9de3a4
JustyneSmagacz <justynesmagacz@gmail.com>
2021-02-08 18:09:40 +0100

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -3,9 +3,14 @@ import './App.css';
 
 
 export default function App() {
-  return <div className="App"> Weather App
+  return (
+  <div className="App"> 
+    <div className="Container">
 
-  <footer>Open Source on <a href="https://github.com/JustyneSmagacz/weather-react" target="_blank">Github</a> | Coded by <a href="https://www.linkedin.com/in/justyne-smagacz-aa6b4a31/" target="_blank">Justyne Smagacz</a></footer>
-  
-  </div>;
+
+      <h1>Weather App</h1>
+
+      <footer>Open Source on <a href="https://github.com/JustyneSmagacz/weather-react" target="_blank">Github</a> | Coded by <a href="https://www.linkedin.com/in/justyne-smagacz-aa6b4a31/" target="_blank">Justyne Smagacz</a></footer>
+    </div>
+  </div>);
 }
