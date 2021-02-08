2b80c32eed4b2790b2a91d9f300eb177b83dd6be
JustyneSmagacz <justynesmagacz@gmail.com>
2021-02-08 17:52:04 +0100

    Added Github URL/ Footer

--- src/App.js
+++ src/App.js
@@ -2,8 +2,10 @@ import React from "react";
 import './App.css';
 
 
-function App() {
-  return <div className="App"> Weather App</div>;
-}
+export default function App() {
+  return <div className="App"> Weather App
 
-export default App;
+  <footer>Open Source on <a href="https://github.com/JustyneSmagacz/weather-react" target="_blank">Github</a> | Coded by <a href="https://www.linkedin.com/in/justyne-smagacz-aa6b4a31/" target="_blank">Justyne Smagacz</a></footer>
+  
+  </div>;
+}
