89b2f11fc67864a5cae2e8b7813e605d414f7e45
JustyneSmagacz <justynesmagacz@gmail.com>
2021-02-08 17:45:14 +0100

    Cleaning up the mess I made

--- src/App.js
+++ src/App.js
@@ -1,18 +1,9 @@
+import React from "react";
 import './App.css';
-import Weather from "./Weather.js";
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-
-        <h1>
-          Hello World from React!
-        </h1>
-      <Weather city="Madrid" country="Spain"/>
-      </header>
-    </div>
-  );
+  return <div className="App"> Weather App</div>;
 }
 
 export default App;
