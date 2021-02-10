45273c778736ff0ddbf7b8f608ee784bffa1c3c7
JustyneSmagacz <justynesmagacz@gmail.com>
2021-02-10 17:11:32 +0100

    checking to see if Weather.js is removed

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from "react";
-import Weather from "./Weather.js";
+import SearchEngine from "./SearchEngine.js";
 import './App.css';
-
 
 
 export default function App() {
@@ -9,7 +8,7 @@ export default function App() {
   <div className="App"> 
     <div className="Container">
 
-      <Weather defaultCity="Madrid"/>
+      <SearchEngine defaultCity="Madrid"/>
       
       <footer>Open Source on <a href="https://github.com/JustyneSmagacz/weather-react" target="_blank">Github</a> | Coded by <a href="https://www.linkedin.com/in/justyne-smagacz-aa6b4a31/" target="_blank">Justyne Smagacz</a></footer>
     </div>
