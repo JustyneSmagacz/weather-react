74e70193e9abd95f65a16b551031b01b8e4a9f0d
JustyneSmagacz <justynesmagacz@gmail.com>
2021-02-10 19:39:38 +0100

    deleted WeatherPreview

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
-import SearchEngine from "./SearchEngine.js";
+import SearchEngine from "./SearchEngine";
 import './App.css';
+import WeatherIcon from "./WeatherIcon"
 
 
 export default function App() {
@@ -9,6 +10,7 @@ export default function App() {
     <div className="Container">
 
       <SearchEngine defaultCity="Madrid"/>
+      <WeatherIcon code={props.data.weather[0].icon} />
       
       <footer>Open Source on <a href="https://github.com/JustyneSmagacz/weather-react" target="_blank" rel="noreferrer">Github</a> | Coded by <a href="https://www.linkedin.com/in/justyne-smagacz-aa6b4a31/" target="_blank" rel="noreferrer">Justyne Smagacz</a></footer>
     </div>
