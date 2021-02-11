89937ff52d7535a4c50aa864b0cd5ad5ef370e00
JustyneSmagacz <justynesmagacz@gmail.com>
2021-02-11 09:19:49 +0100

    moved WeatherIcon from app.js to WeatherDay

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from "react";
 import SearchEngine from "./SearchEngine";
 import './App.css';
-import WeatherIcon from "./WeatherIcon"
 
 
 export default function App() {
@@ -10,7 +9,7 @@ export default function App() {
     <div className="Container">
 
       <SearchEngine defaultCity="Madrid"/>
-      <WeatherIcon code={props.data.weather[0].icon} />
+     
       
       <footer>Open Source on <a href="https://github.com/JustyneSmagacz/weather-react" target="_blank" rel="noreferrer">Github</a> | Coded by <a href="https://www.linkedin.com/in/justyne-smagacz-aa6b4a31/" target="_blank" rel="noreferrer">Justyne Smagacz</a></footer>
     </div>
