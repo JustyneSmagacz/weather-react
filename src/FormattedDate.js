727733cd3423ac60d548e577f77e08fb33a3d63e
JustyneSmagacz <justynesmagacz@gmail.com>
2021-02-11 13:03:55 +0100

    added Number to date

--- src/FormattedDate.js
+++ src/FormattedDate.js
@@ -3,11 +3,12 @@ import React from "react";
 export default function FormattedDate (props){
     let days =["Sun", "Mon", "Tues", "Wed", "Thurs", "Fri", "Sat"]
     let months =["Jan", "Feb", "Mar", "Apr", "May", "June", "July", "Aug", "Sept", "Oct", "Nov", "Dec"]
+    let date = props.date.getDate();
     let day = days[props.date.getDay()];
     let month = months[props.date.getMonth()];
     return (
     <div>
-        {day}, {month} 
+        {day},{date} {month} 
     </div>
     );
 }
